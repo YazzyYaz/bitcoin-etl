ff30037f96d6948acd9e9c61274809d0ddd43ab0
Yaz Khoury <yaz.khoury@gmail.com>
2019-02-23 19:28:08 -0500

    feat: Add bgold integration

--- bitcoinetl/enum/chain.py
+++ bitcoinetl/enum/chain.py
@@ -1,11 +1,12 @@
 class Chain:
     BITCOIN = 'bitcoin'
     BITCOIN_CASH = 'bitcoin_cash'
+    BITCOIN_GOLD = 'bitcoin_gold'
     DOGECOIN = 'dogecoin'
     LITECOIN = 'litecoin'
     DASH = 'dash'
     ZCASH = 'zcash'
 
-    ALL = [BITCOIN, BITCOIN_CASH, DOGECOIN, LITECOIN, DASH, ZCASH]
+    ALL = [BITCOIN, BITCOIN_CASH, BITCOIN_GOLD, DOGECOIN, LITECOIN, DASH, ZCASH]
     # Old API doesn't support verbosity for getblock which doesn't allow querying all transactions in a block in 1 go.
     HAVE_OLD_API = [BITCOIN_CASH, DOGECOIN, DASH]
